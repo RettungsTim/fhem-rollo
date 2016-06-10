8cad61b7a7bb85be0424fed3164182ddf1fad434
Thomas Ramm <post@thomas-ramm.de>
2016-06-10 22:05:08 +0200

    Lizenzvermerk hinzugefügt

--- FHEM/44_ROLLO.pm
+++ FHEM/44_ROLLO.pm
@@ -5,8 +5,24 @@
 # Thomas Ramm, 2016
 # Tim Horenkamp, 2016
 #
-############################################################
-
+########################################################################################
+#
+#  This programm is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#  A copy is found in the textfile GPL.txt and important notices to the license
+#  from the author is found in LICENSE.txt distributed with these scripts.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+########################################################################################
 package main;
 
 use strict;
